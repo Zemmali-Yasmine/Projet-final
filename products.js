e4cbe9e14255ca0dfc0ca16722c95fa7103847c6
Zemmali-Yasmine <zemmaliyasmine@gmail.com>
2024-12-20 22:50:30 +0000

    v4

--- products.js
+++ products.js
@@ -1,215 +1,215 @@
 export const products = [
-    //bracelet
-    {
-        id: 'b1',
-        name: 'Golden Chain Bracelet',
-        price: 145000,
-        category: 'bracelets',
-        image: 'images/bracelets/b1.jpg',
-        isNew: false
-    },
-    {
-        id: 'b2',
-        name: 'Diamond Tennis Bracelet',
-        price: 100000,
-        category: 'bracelets',
-        image: 'images/bracelets/b2.jpg',
-        isNew: false
-    },
-    {
-        id: 'b3',
-        name: 'Pearl Charm Bracelet',
-        price: 25000,
-        category: 'bracelets',
-        image: 'images/bracelets/b3.jpg',
-        isNew: true
-    },
-    {
-        id: 'b4',
-        name: 'Rose Gold Bangle',
-        price: 35000,
-        category: 'bracelets',
-        image: 'images/bracelets/b4.jpg',
-        isNew: false
-    },
-    {
-        id: 'b5',
-        name: 'Sapphire Link Bracelet',
-        price: 80000,
-        category: 'bracelets',
-        image: 'images/bracelets/b5.jpg',
-        isNew: true
-    },
-    {
-        id: 'b6',
-        name: 'Silver Charm Bracelet',
-        price: 24500,
-        category: 'bracelets',
-        image: 'images/bracelets/b6.jpg',
-        isNew: false
-    },
-    // Rings
-    {
-        id: 'r1',
-        name: 'Diamond Solitaire Ring',
-        price: 75000,
-        category: 'rings',
-        image: 'images/rings/r1.jpg',
-        isNew: false
-    },
-    {
-        id: 'r2',
-        name: 'Gold Wedding Band',
-        price: 36000,
-        category: 'rings',
-        image: 'images/rings/r2.jpg',
-        isNew: false
-    },
-    {
-        id: 'r3',
-        name: 'Emerald Halo Ring',
-        price: 60000,
-        category: 'rings',
-        image: 'images/rings/r3.jpg',
-        isNew: false
-    },
-    {
-        id: 'r4',
-        name: 'Ruby Vintage Ring',
-        price: 76000,
-        category: 'rings',
-        image: 'images/rings/r4.jpg',
-        isNew: false
-    },
-    {
-        id: 'r5',
-        name: 'Sapphire Engagement Ring',
-        price: 187500,
-        category: 'rings',
-        image: 'images/rings/r5.jpg',
-        isNew: true
-    },
-    {
-        id: 'r6',
-        name: 'Pearl Statement Ring',
-        price: 29950,
-        category: 'rings',
-        image: 'images/rings/r6.jpg',
-        isNew: false
-    },
-    // Necklaces
-    {
-        id: 'n1',
-        name: 'Diamond Pendant Necklace',
-        price: 79999,
-        category: 'necklaces',
-        image: 'images/necklaces/n1.jpg',
-        isNew: true
-    },
-    {
-        id: 'n2',
-        name: 'Pearl Strand Necklace',
-        price: 149365,
-        category: 'necklaces',
-        image: 'images/necklaces/n2.jpg',
-        isNew: false
-    },
-    {
-        id: 'n3',
-        name: 'Gold Chain Necklace',
-        price: 139870,
-        category: 'necklaces',
-        image: 'images/necklaces/n3.jpg',
-        isNew: false
-    },
-    {
-        id: 'n4',
-        name: 'Emerald Choker',
-        price: 59900,
-        category: 'necklaces',
-        image: 'images/necklaces/n4.jpg',
-        isNew: true
-    },
-    {
-        id: 'n5',
-        name: 'Ruby Pendant',
-        price: 88888,
-        category: 'necklaces',
-        image: 'images/necklaces/n5.jpg',
-        isNew: false
-    },
-    {
-        id: 'n6',
-        name: 'Silver Lariat Necklace',
-        price: 26200,
-        category: 'necklaces',
-        image: 'images/necklaces/n6.jpg',
-        isNew: false
-    },
-    // Earrings
-    {
-        id: 'e1',
-        name: 'Diamond Stud Earrings',
-        price: 249100,
-        category: 'earrings',
-        image: 'images/earrings/e1.jpg',
-        isNew: true
-    },
-    {
-        id: 'e2',
-        name: 'Pearl Drop Earrings',
-        price: 29274,
-        category: 'earrings',
-        image: 'images/earrings/e2.jpg',
-        isNew: false
-    },
-    {
-        id: 'e3',
-        name: 'Gold Hoop Earrings',
-        price: 74245,
-        category: 'earrings',
-        image: 'images/earrings/e3.jpg',
-        isNew: false
-    },
-    {
-        id: 'e4',
-        name: 'Sapphire Chandelier Earrings',
-        price: 49499,
-        category: 'earrings',
-        image: 'images/earrings/e4.jpg',
-        isNew: false
-    },
-    {
-        id: 'e5',
-        name: 'Ruby Cluster Earrings',
-        price: 54381,
-        category: 'earrings',
-        image: 'images/earrings/e5.jpg',
-        isNew: true
-    },
-    {
-        id: 'e6',
-        name: 'Silver Dangle Earrings',
-        price: 25000,
-        category: 'earrings',
-        image: 'images/earrings/e6.jpg',
-        isNew: true
-    },
-    // Sets
-    {
-        id: 's1',
-        name: 'Diamond Wedding Set',
-        price: 250000,
-        category: 'sets',
-        image: 'images/sets/s1.jpg',
-        isNew: true
-    },
-    {
-        id: 's2',
-        name: 'Pearl Jewelry Set',
-        price: 150000,
-        category: 'sets',
-        image: 'images/sets/s2.jpg',
-        isNew: true
-    },
+  //bracelet
+  {
+    id: "b1",
+    name: "Golden Chain Bracelet",
+    price: 145000,
+    category: "bracelets",
+    image: "images/bracelets/b1.jpg",
+    isNew: false,
+  },
+  {
+    id: "b2",
+    name: "Diamond Tennis Bracelet",
+    price: 100000,
+    category: "bracelets",
+    image: "images/bracelets/b2.jpg",
+    isNew: false,
+  },
+  {
+    id: "b3",
+    name: "Pearl Charm Bracelet",
+    price: 25000,
+    category: "bracelets",
+    image: "images/bracelets/b3.jpg",
+    isNew: true,
+  },
+  {
+    id: "b4",
+    name: "Rose Gold Bangle",
+    price: 35000,
+    category: "bracelets",
+    image: "images/bracelets/b4.jpg",
+    isNew: false,
+  },
+  {
+    id: "b5",
+    name: "Sapphire Link Bracelet",
+    price: 80000,
+    category: "bracelets",
+    image: "images/bracelets/b5.jpg",
+    isNew: true,
+  },
+  {
+    id: "b6",
+    name: "Silver Charm Bracelet",
+    price: 24500,
+    category: "bracelets",
+    image: "images/bracelets/b6.jpg",
+    isNew: false,
+  },
+  // Rings
+  {
+    id: "r1",
+    name: "Diamond Solitaire Ring",
+    price: 75000,
+    category: "rings",
+    image: "images/rings/r1.jpg",
+    isNew: false,
+  },
+  {
+    id: "r2",
+    name: "Gold Wedding Band",
+    price: 36000,
+    category: "rings",
+    image: "images/rings/r2.jpg",
+    isNew: false,
+  },
+  {
+    id: "r3",
+    name: "Emerald Halo Ring",
+    price: 60000,
+    category: "rings",
+    image: "images/rings/r3.jpg",
+    isNew: false,
+  },
+  {
+    id: "r4",
+    name: "Ruby Vintage Ring",
+    price: 76000,
+    category: "rings",
+    image: "images/rings/r4.jpg",
+    isNew: false,
+  },
+  {
+    id: "r5",
+    name: "Sapphire Engagement Ring",
+    price: 187500,
+    category: "rings",
+    image: "images/rings/r5.jpg",
+    isNew: true,
+  },
+  {
+    id: "r6",
+    name: "Pearl Statement Ring",
+    price: 29950,
+    category: "rings",
+    image: "images/rings/r6.jpg",
+    isNew: false,
+  },
+  // Necklaces
+  {
+    id: "n1",
+    name: "Diamond Pendant Necklace",
+    price: 79999,
+    category: "necklaces",
+    image: "images/necklaces/n1.jpg",
+    isNew: true,
+  },
+  {
+    id: "n2",
+    name: "Pearl Strand Necklace",
+    price: 149365,
+    category: "necklaces",
+    image: "images/necklaces/n2.jpg",
+    isNew: false,
+  },
+  {
+    id: "n3",
+    name: "Gold Chain Necklace",
+    price: 139870,
+    category: "necklaces",
+    image: "images/necklaces/n3.jpg",
+    isNew: false,
+  },
+  {
+    id: "n4",
+    name: "Emerald Choker",
+    price: 59900,
+    category: "necklaces",
+    image: "images/necklaces/n4.jpg",
+    isNew: true,
+  },
+  {
+    id: "n5",
+    name: "Ruby Pendant",
+    price: 88888,
+    category: "necklaces",
+    image: "images/necklaces/n5.jpg",
+    isNew: false,
+  },
+  {
+    id: "n6",
+    name: "Silver Lariat Necklace",
+    price: 26200,
+    category: "necklaces",
+    image: "images/necklaces/n6.jpg",
+    isNew: false,
+  },
+  // Earrings
+  {
+    id: "e1",
+    name: "Diamond Stud Earrings",
+    price: 249100,
+    category: "earrings",
+    image: "images/earrings/e1.jpg",
+    isNew: true,
+  },
+  {
+    id: "e2",
+    name: "Pearl Drop Earrings",
+    price: 29274,
+    category: "earrings",
+    image: "images/earrings/e2.jpg",
+    isNew: false,
+  },
+  {
+    id: "e3",
+    name: "Gold Hoop Earrings",
+    price: 74245,
+    category: "earrings",
+    image: "images/earrings/e3.jpg",
+    isNew: false,
+  },
+  {
+    id: "e4",
+    name: "Sapphire Chandelier Earrings",
+    price: 49499,
+    category: "earrings",
+    image: "images/earrings/e4.jpg",
+    isNew: false,
+  },
+  {
+    id: "e5",
+    name: "Ruby Cluster Earrings",
+    price: 54381,
+    category: "earrings",
+    image: "images/earrings/e5.jpg",
+    isNew: true,
+  },
+  {
+    id: "e6",
+    name: "Silver Dangle Earrings",
+    price: 25000,
+    category: "earrings",
+    image: "images/earrings/e6.jpg",
+    isNew: true,
+  },
+  // Sets
+  {
+    id: "s1",
+    name: "Diamond Wedding Set",
+    price: 250000,
+    category: "sets",
+    image: "images/sets/s1.jpg",
+    isNew: true,
+  },
+  {
+    id: "s2",
+    name: "Pearl Jewelry Set",
+    price: 150000,
+    category: "sets",
+    image: "images/sets/s2.jpg",
+    isNew: true,
+  },
 ];
